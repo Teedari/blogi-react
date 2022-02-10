6a0863e3c8d7a38ccd44d22e27cc651697977bf5
Teedari <godfredderi47@gmail.com>
2022-02-10 23:47:34 +0000

    completed read article page

--- src/pages/ReadArticle/index.jsx
+++ src/pages/ReadArticle/index.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import CustomLayout from '../../layout'
-import snowImage from '../../assets/images/snow.jpg'
+import React from "react";
+import CustomLayout from "../../layout";
+import snowImage from "../../assets/images/snow.jpg";
+import { Avatar, Typography } from "antd";
 const ReadArticle = () => {
   return (
     <CustomLayout>
-      <div className="article-read-hero-section position-relative">
-        asdfa
-        <div className='image-background overlap-img'>
+      <div className="article-read-hero-section position-relative d-flex flex-column justify-content-end pb-5 text-white">
+        {/* <div className='image-background' style={{zIndex: '1'}}>
           <img src={snowImage} alt="" />
+        </div> */}
+        <div className="container ">
+          <p>
+            John / Inspiration /
+            <span>Your Most Burning Questions About Light Lamp</span>
+          </p>
+          <hr />
+          <Typography.Title>
+            Your Most Burning Questions About Ligh Lamp
+          </Typography.Title>
+          <p>
+            <Avatar src={snowImage} />
+            <small className="text-muted">John Doe</small>
+          </p>
+        </div>
+      </div>
+      <div className='py-4'>
+        <div className="container">
+          <p>
+            Quis mollit voluptate sit qui quis duis ipsum labore deserunt
+            commodo duis non laborum aliquip. In do adipisicing do Lorem qui
+            quis labore sint est exercitation. Proident commodo velit ex id
+            reprehenderit. Aliqua exercitation ut reprehenderit aute fugiat eu
+            do consectetur. Sint fugiat pariatur id
+          </p>
+          <p>Ullamco ad occaecat sunt eu pariatur aliquip.
+          Ullamco ad occaecat sunt eu pariatur aliquip.
+          Ullamco ad occaecat sunt eu pariatur aliquip.
+          Ullamco ad occaecat sunt eu pariatur aliquip.
+          Ullamco ad occaecat sunt eu pariatur aliquip.
+          </p>
         </div>
       </div>
     </CustomLayout>
-  )
-}
+  );
+};
 
-export default ReadArticle
+export default ReadArticle;
