696a7189733c5a2b404f78c2b35f1d49bb7d1052
Teedari <godfredderi47@gmail.com>
2022-02-10 23:04:07 +0000

    added read article page

---
+++ src/pages/ReadArticle/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CustomLayout from '../../layout'
+
+const ReadArticle = () => {
+  return (
+    <CustomLayout>ReadArticle</CustomLayout>
+  )
+}
+
+export default ReadArticle
