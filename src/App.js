450f9f59583da5b5abea35ab04c3a5be5f4603dd
Teedari <godfredderi47@gmail.com>
2022-02-10 23:05:34 +0000

    added url to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/Login";
 import PostDetail from "./pages/PostDetail";
+import ReadArticle from "./pages/ReadArticle";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/posts/:ID" element={<PostDetail />} />
+          <Route path="/post/read" element={<ReadArticle />} />
           <Route path="*" element={<>WRONG URL</>} />
         </Routes>
       </Router>
